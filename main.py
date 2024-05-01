9b354ce44f01362ff55320a19a447c8b8aee70ca
Carlos Fernando <pb22007@ues.edu.sv>
2024-05-01 16:30:36 -0600

    eoooo

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     return render_template('index.html')
 
+@app.route('/metodos/directos/cerrados/Biseccion', methods=['GET'])
+def biseccion():
+    return render_template('Biseccion.html')
+
+@app.route('/metodos/directos/cerrados/Biseccion', methods=['POST'])
+def prueba():
+    print(request.json)
+    return jsonify({'result': 'success', 'data': 'Hola Mundo'})
+
 if __name__ == '__main__':
     app.run()
