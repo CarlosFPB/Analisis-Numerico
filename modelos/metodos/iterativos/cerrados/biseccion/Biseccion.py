4c1bff7da1d7d12ee92413eafbbed25ba4cbaa90
Carlos Fernando <pb22007@ues.edu.sv>
2024-05-03 14:25:01 -0600

    metodos de prueba

--- modelos/metodos/iterativos/cerrados/biseccion/Biseccion.py
+++ modelos/metodos/iterativos/cerrados/biseccion/Biseccion.py
@@ -1,6 +1,6 @@
 import sympy as sp
 from flask import jsonify
-from .....extras.funciones import errores, biseccion, respuesta_json
+from .....extras.Funciones import errores, biseccion, respuesta_json
 
 class medoto_biseccion():
 
--- modelos/metodos/iterativos/cerrados/falsa_posicion/Falsa_posicion.py
+++ modelos/metodos/iterativos/cerrados/falsa_posicion/Falsa_posicion.py
@@ -1,32 +1,29 @@
 import sympy as sp
-from .....extras.funciones import errores, falsaPosicion, respuesta_json
+from .....extras.Funciones import errores, falsaPosicion, respuesta_json
 
 
 class falsa_posicion():
 
     def calcular_falsa_posicion(json_data):
         x = sp.symbols('x')
-        f_x = sp.E**-x - x
 
         #instancio las respuest json
         instancia_respuesta = respuesta_json()
 
-        error_aceptable = errores.error_de_tolerancia(3)
-        x1 = 0
-        xu = 1
+        #obtengo los valores del json
+        f_x = sp.sympify(json_data["funcion"])
+        error_aceptable = float(json_data["tolerancia"])
+        x1 = float(json_data["xi"])
+        xu = float(json_data["xu"])
         xr = 0
-        valor_real = 0.56714
-
-        iteracion =1
-        xr = falsaPosicion.primera_aproximacion(f_x,x1,xu)
-        valor_anterior = xr
 
         while True:
-            
+            iteracion =1
+            valor_anterior = xr
+            xr = falsaPosicion.primera_aproximacion(f_x,x1,xu)
             #primera aproximacion
             evaluacion = falsaPosicion.multiplicacion_evaluadas(f_x,x1,xr)
             if evaluacion > 0:
-                
                 x1 = xr
             elif evaluacion < 0:
                 xu = xr
@@ -40,9 +37,6 @@ class falsa_posicion():
                 if error_acumulado < error_aceptable:
                     break
                 
-            iteracion +=1
-            valor_anterior = xr
-            xr = falsaPosicion.primera_aproximacion(f_x,x1,xu)
 
 
 
