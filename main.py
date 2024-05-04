2a1b4d8e9706b15b3a4e0b38eb8d813019109aa1
Carlos Fernando <pb22007@ues.edu.sv>
2024-05-04 14:53:13 -0600

    metodo lineal y cuadratico

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 from modelos.metodos.iterativos.cerrados.biseccion.Biseccion import medoto_biseccion
 from modelos.metodos.directos.polinomicas_mayor_3.ferrari.Ferrari import metodo_ferrari
 from  modelos.metodos.directos.polinomicas_mayor_3.tartaglia.Tartaglia import metodo_tartaglia
+from modelos.metodos.directos.polinomicas_menor_2.cuadratica.Cuadratica import metodo_cuadratico
+from modelos.metodos.directos.polinomicas_menor_2.lineal.Lineal import metodo_lineal
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -40,6 +42,26 @@ def renderizar_tartaglia():
 def calcular_tartaglia():
     json_data = request.json
     respuesta = metodo_tartaglia.calcular_tartaglia(json_data)
+    return respuesta
+
+@app.route('/metodos/directos/gradomenos2/cuartica', methods=['GET'])
+def renderizar_cuartica():
+    return render_template('Cuartica.html')
+
+@app.route('/metodos/directos/gradomenos2/cuartica', methods=['POST'])
+def calcular_cuartica():
+    json_data = request.json
+    respuesta = metodo_cuadratico.calcular_cuadratico(json_data)
+    return respuesta
+
+@app.route('/metodos/directos/gradomenos2/lineal', methods=['GET'])
+def renderizar_lineal():
+    return render_template('Lineal.html')
+
+@app.route('/metodos/directos/gradomenos2/lineal', methods=['POST'])
+def calcular_lineal():
+    json_data = request.json
+    respuesta = metodo_lineal.calcular_lineal(json_data)
     return respuesta
 
 if __name__ == '__main__':
--- modelos/metodos/directos/polinomicas_menor_2/cuadratica/Cuadratica.py
+++ modelos/metodos/directos/polinomicas_menor_2/cuadratica/Cuadratica.py
@@ -1,13 +1,39 @@
 import sympy as sp
 from modelos.extras.Funciones import  respuesta_json
+from flask import jsonify
 
 class metodo_cuadratico():
     def calcular_cuadratico(json_data):
         x = sp.symbols('x')
-        f_x = sp.simplify(json_data['funcion'])
+        instancia_respuesta = respuesta_json()
+        try:
+            f_x = sp.sympify(json_data["funcion"])
+        except:
+            return jsonify({"error":"Error en la funcion"})
         grado = f_x.as_poly().degree()
         if(grado == 2):
-            solucion = sp.solve(f_x, x)
-            return respuesta_json(solucion)
+            instancia_respuesta.agregar_titulo1("Metodo Cuadratico")
+            instancia_respuesta.agregar_parrafo("Este metodo nos sirve para encontrar la raiz de una ecuacion, para ello se necesita una funcion f(x) de grado 2 y usar formula cuadratica.")
+            instancia_respuesta.agregar_titulo1("Valores Iniciales")
+            instancia_respuesta.agregar_parrafo(f"Funcion: {f_x}")
+            instancia_respuesta.agregar_titulo1("Usando la formula cuadratica")
+            instancia_respuesta.agregar_parrafo("La formula cuadratica es: x = (-b ± √(b^2 - 4ac)) / 2a")
+            instancia_respuesta.agregar_parrafo("Donde a, b y c son los coeficientes de la funcion cuadratica")
+            polinomio = f_x.as_poly(x)
+            grado = polinomio.degree()
+            coeficientes = [polinomio.coeff_monomial(x**i) for i in range(grado, -1, -1)]#aunq haya 0
+            a = coeficientes[0]
+            b = coeficientes[1]
+            c = coeficientes[2]
+            instancia_respuesta.agregar_parrafo(f"Coeficientes: a = {a}, b = {b}, c = {c}")
+            instancia_respuesta.agregar_titulo1("Calculando las raices")
+            instancia_respuesta.agregar_parrafo(f"Sustituyendo : x = (-{b} ± √({b}^2 - 4*{a}*{c})) / 2*{a}")
+            resp = (-b + sp.sqrt(b**2 - 4*a*c)) / 2*a
+            resp2 = (-b - sp.sqrt(b**2 - 4*a*c)) / 2*a
+            instancia_respuesta.agregar_clave_valor("Raiz 1:",f"x = {resp}")
+            instancia_respuesta.agregar_clave_valor("Raiz 2:",f"x = {resp2}")
+            respuesta = instancia_respuesta.obtener_y_limpiar_respuesta()
+            return jsonify(respuesta)
         else:
-            return respuesta_json("La función no es cuadrática")
+            #manejar excepciones
+            return jsonify({"error":"La funcion no es de grado 1"})
