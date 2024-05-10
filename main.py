fdcf9d4e616a4c533904d4517ea3bb0c56e02fb3
Carlos Fernando <pb22007@ues.edu.sv>
2024-05-09 19:49:52 -0600

    adaptado a vercel

--- main.py
+++ main.py
@@ -14,132 +14,130 @@ from modelos.metodos.iterativos.abiertos.secante.Secante import metodo_secante
 from modelos.metodos.iterativos.polinomicos.horner.Horner import metodo_horner
 from modelos.metodos.iterativos.polinomicos.muller.Muller import metodo_muller
 
-def crear_app():
-    app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-    
-    @app.errorhandler(404)
-    def page_not_found(e):
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
     # Redirige automáticamente a la URL de la imagen del gato
-        return redirect("https://http.cat/404", code=302)
+    return redirect("https://http.cat/404", code=302)
 
-    @app.route('/metodos/iterativos/cerrados/Biseccion', methods=['GET'])
-    def renderizar_biseccion():
-        return render_template('Biseccion.html')
+@app.route('/metodos/iterativos/cerrados/Biseccion', methods=['GET'])
+def renderizar_biseccion():
+    return render_template('Biseccion.html')
 
-    @app.route('/metodos/iterativos/cerrados/Biseccion', methods=['POST'])
-    def calcular_biseccion():
-        json_data = request.json
-        respuesta = medoto_biseccion.calcular_biseccion(json_data)
-        return respuesta
+@app.route('/metodos/iterativos/cerrados/Biseccion', methods=['POST'])
+def calcular_biseccion():
+    json_data = request.json
+    respuesta = medoto_biseccion.calcular_biseccion(json_data)
+    return respuesta
 
-    @app.route('/metodos/directos/gradomas3/Ferrari', methods=['GET'])
-    def renderizar_ferrari():
-        return render_template('Ferrari.html')
+@app.route('/metodos/directos/gradomas3/Ferrari', methods=['GET'])
+def renderizar_ferrari():
+    return render_template('Ferrari.html')
 
-    @app.route('/metodos/directos/gradomas3/Ferrari', methods=['POST'])
-    def calcular_ferrari():
-        json_data = request.json
-        respuesta = metodo_ferrari.calcular_ferrari(json_data)
-        return respuesta
+@app.route('/metodos/directos/gradomas3/Ferrari', methods=['POST'])
+def calcular_ferrari():
+    json_data = request.json
+    respuesta = metodo_ferrari.calcular_ferrari(json_data)
+    return respuesta
 
-    @app.route('/metodos/directos/gradomas3/Tartaglia', methods=['GET'])
-    def renderizar_tartaglia():
-        return render_template('Tartaglia.html')
+@app.route('/metodos/directos/gradomas3/Tartaglia', methods=['GET'])
+def renderizar_tartaglia():
+    return render_template('Tartaglia.html')
 
-    @app.route('/metodos/directos/gradomas3/Tartaglia', methods=['POST'])
-    def calcular_tartaglia():
-        json_data = request.json
-        respuesta = metodo_tartaglia.calcular_tartaglia(json_data)
-        return respuesta
+@app.route('/metodos/directos/gradomas3/Tartaglia', methods=['POST'])
+def calcular_tartaglia():
+    json_data = request.json
+    respuesta = metodo_tartaglia.calcular_tartaglia(json_data)
+    return respuesta
 
-    @app.route('/metodos/directos/gradomenos2/cuartica', methods=['GET'])
-    def renderizar_cuartica():
-        return render_template('Cuadratica.html')
+@app.route('/metodos/directos/gradomenos2/cuartica', methods=['GET'])
+def renderizar_cuartica():
+    return render_template('Cuadratica.html')
 
-    @app.route('/metodos/directos/gradomenos2/cuartica', methods=['POST'])
-    def calcular_cuartica():
-        json_data = request.json
-        respuesta = metodo_cuadratico.calcular_cuadratico(json_data)
-        return respuesta
+@app.route('/metodos/directos/gradomenos2/cuartica', methods=['POST'])
+def calcular_cuartica():
+    json_data = request.json
+    respuesta = metodo_cuadratico.calcular_cuadratico(json_data)
+    return respuesta
 
-    @app.route('/metodos/directos/gradomenos2/lineal', methods=['GET'])
-    def renderizar_lineal():
-        return render_template('Lineal.html')
+@app.route('/metodos/directos/gradomenos2/lineal', methods=['GET'])
+def renderizar_lineal():
+    return render_template('Lineal.html')
 
-    @app.route('/metodos/directos/gradomenos2/lineal', methods=['POST'])
-    def calcular_lineal():
-        json_data = request.json
-        respuesta = metodo_lineal.calcular_lineal(json_data)
-        return respuesta
+@app.route('/metodos/directos/gradomenos2/lineal', methods=['POST'])
+def calcular_lineal():
+    json_data = request.json
+    respuesta = metodo_lineal.calcular_lineal(json_data)
+    return respuesta
 
-    @app.route('/metodos/directos/abiertos/punto-Fijo', methods=['GET'])
-    def Punto_fijo():
-        return render_template('Punto_fijo.html')
+@app.route('/metodos/directos/abiertos/punto-Fijo', methods=['GET'])
+def Punto_fijo():
+    return render_template('Punto_fijo.html')
 
-    @app.route('/metodos/directos/abiertos/punto-Fijo', methods=['POST'])
-    def calcular_punto_fijo():
-        json_data = request.json
-        respuesta = metodo_punto_fijo.calcular_punto_fijo(json_data)
-        return respuesta    
+@app.route('/metodos/directos/abiertos/punto-Fijo', methods=['POST'])
+def calcular_punto_fijo():
+    json_data = request.json
+    respuesta = metodo_punto_fijo.calcular_punto_fijo(json_data)
+    return respuesta    
 
-    @app.route('/metodos/directos/abiertos/newton', methods=['GET'])
-    def Newton():
-        return render_template('Newton.html')
+@app.route('/metodos/directos/abiertos/newton', methods=['GET'])
+def Newton():
+    return render_template('Newton.html')
 
-    @app.route('/metodos/directos/abiertos/newton', methods=['POST'])
-    def calcular_newton():
-        json_data = request.json
-        respuesta = metodo_newton.calcular_newton(json_data)
-        return respuesta
+@app.route('/metodos/directos/abiertos/newton', methods=['POST'])
+def calcular_newton():
+    json_data = request.json
+    respuesta = metodo_newton.calcular_newton(json_data)
+    return respuesta
 
-    @app.route('/metodos/directos/abiertos/Newton-Modificado', methods=['GET'])
-    def Newton_Modificado():
-        return render_template('Newton_modificado.html')
+@app.route('/metodos/directos/abiertos/Newton-Modificado', methods=['GET'])
+def Newton_Modificado():
+    return render_template('Newton_modificado.html')
 
-    @app.route('/metodos/directos/abiertos/Newton-Modificado', methods=['POST'])
-    def calcular_newton_modificado():
-        json_data = request.json
-        respuesta = metodo_newton_modificado.calcular_newton_modificado(json_data)
-        return respuesta
+@app.route('/metodos/directos/abiertos/Newton-Modificado', methods=['POST'])
+def calcular_newton_modificado():
+    json_data = request.json
+    respuesta = metodo_newton_modificado.calcular_newton_modificado(json_data)
+    return respuesta
 
-    @app.route('/metodos/directos/abiertos/Secante', methods=['GET'])
-    def Secante():
-        return render_template('Secante.html')
+@app.route('/metodos/directos/abiertos/Secante', methods=['GET'])
+def Secante():
+    return render_template('Secante.html')
 
-    @app.route('/metodos/directos/abiertos/Secante', methods=['POST'])
-    def calcular_secante():
-        json_data = request.json
-        respuesta = metodo_secante.calcular_secante(json_data)
-        return respuesta
+@app.route('/metodos/directos/abiertos/Secante', methods=['POST'])
+def calcular_secante():
+    json_data = request.json
+    respuesta = metodo_secante.calcular_secante(json_data)
+    return respuesta
 
-    @app.route('/metodos/iterativos/polinomicos/horner', methods=['POST'])
-    def calcular_horner():
-        json_data = request.json
-        respuesta = metodo_horner.calcular_Horner(json_data)
-        return respuesta
-    
-    @app.route('/metodos/iterativos/polinomicos/horner', methods=['GET'])
-    def Horner():
-        return render_template('Horner.html')
-    
-    @app.route('/metodos/iterativos/polinomicos/muller', methods=['POST'])
-    def calcular_muller():
-        json_data = request.json
-        respuesta = metodo_muller.calcular_Muller(json_data)
-        return respuesta
-    
-    @app.route('/metodos/iterativos/polinomicos/muller', methods=['GET'])
-    def Muller():
-        return render_template('Muller.html')
+@app.route('/metodos/iterativos/polinomicos/horner', methods=['POST'])
+def calcular_horner():
+    json_data = request.json
+    respuesta = metodo_horner.calcular_Horner(json_data)
+    return respuesta
+
+@app.route('/metodos/iterativos/polinomicos/horner', methods=['GET'])
+def Horner():
+    return render_template('Horner.html')
+
+@app.route('/metodos/iterativos/polinomicos/muller', methods=['POST'])
+def calcular_muller():
+    json_data = request.json
+    respuesta = metodo_muller.calcular_Muller(json_data)
+    return respuesta
+
+@app.route('/metodos/iterativos/polinomicos/muller', methods=['GET'])
+def Muller():
+    return render_template('Muller.html')
 
 
         
-    return app
+ 
 
 if __name__ == '__main__':
-    app = crear_app()
     app.run(debug=True, port=os.getenv("PORT", default=5000))
