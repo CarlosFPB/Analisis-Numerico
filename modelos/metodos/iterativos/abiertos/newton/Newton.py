38b5660b8fa8c68db32fe246f4f7c004f6a9689d
Carlos Fernando <pb22007@ues.edu.sv>
2024-05-11 20:37:01 -0600

    update de otras validaciones

--- modelos/metodos/iterativos/abiertos/newton/Newton.py
+++ modelos/metodos/iterativos/abiertos/newton/Newton.py
@@ -7,56 +7,73 @@ class metodo_newton():
 
     @staticmethod
     def calcular_newton(json_data):
-        x = sp.symbols("x")
-        #obteniendo los valores del json
-        f_x = sp.sympify(json_data["funcion"])
-        f_prima = sp.diff(f_x)
-        f_prima_prima = sp.diff(f_prima)
-        x_actual = float(json_data["xi"])
-        x_anterior = 0
-        error_aceptado = float(json_data["tolerancia"])
-        error_acomulado = 100
-        iteracion = 0
-
-        #instanciar respuesta json
-        instancia_respuesta = respuesta_json()
-
-        instancia_respuesta.agregar_titulo1("Metodo de Newton")
-        instancia_respuesta.agregar_parrafo("asdas")
-        instancia_respuesta.crear_tabla()
-        instancia_respuesta.agregar_fila(['Iteracion', 'X1', 'f(X1)', 'f\'(X1)', 'X1+1', 'Error'])
-
-        #Encabezados
-       # print("{:<12s}{:<25s}{:<25s}{:<25s}{:<25s}{:<25s}".
-       #        format("Iteracion","X0","F(x0)","F'(x0)","Xi","Ea%"))
-        while True:
-            iteracion += 1
-            f_prima_evaluada = f_prima.subs(x, x_actual)
-            f_x_evaluada = f_x.subs(x, x_actual)
-            x_anterior = x_actual
-            x_actual = newton.aproximacion(f_x_evaluada, f_prima_evaluada, x_anterior)
-            error_acomulado = errores.error_aproximado_porcentual(x_anterior,x_actual)
+        try:
+            x = sp.symbols("x")
+            #instanciar respuesta json
+            instancia_respuesta = respuesta_json()
             
-            instancia_respuesta.agregar_fila([iteracion, x_anterior, f_x_evaluada, f_prima_evaluada, x_actual, error_acomulado])
-            #print("{:<12d}{:<25s}{:<25s}{:<25s}{:<25s}{:<25s}".
-                #format(iteracion,format(x_anterior),format(f_x_evaluada),format(f_prima_evaluada),format(x_actual),format(error_acomulado)))
-            
-            if(error_acomulado < error_aceptado):
-                break
+            #Verificar la funcion obtenida
+            try:
+                #Ecuaion de la funcion
+                f_x = sp.sympify(json_data["funcion"])
+                resultado = f_x.subs(x, 2)
+                if resultado > 0:
+                    pass
+            except sp.SympifyError:
+                resp = instancia_respuesta.responder_error("Error en la funcion ingresada")
+                return jsonify(resp), 400
+            except TypeError as e:
+                resp = instancia_respuesta.responder_error("Error en la funcion ingresada")
+                return jsonify(resp), 400
+                
+            f_prima = sp.diff(f_x)
+            f_prima_prima = sp.diff(f_prima)
+            try:
+                x_actual = float(json_data["xi"])
+                error_aceptado = float(json_data["tolerancia"])
+            except ValueError as e:
+                resp = instancia_respuesta.responder_error("Error en los datos ingresados" + str(e))
+                return jsonify(resp), 400
+            x_anterior = 0
+            error_acomulado = 100
+            iteracion = 0
 
-            #evaluar el criterio de convergencia
-            f_prima_evaluada = f_prima.subs(x, x_actual)
-            f_prima_prima_evaluada = f_prima_prima.subs(x, x_actual)
-            f_x_evaluada = f_x.subs(x, x_actual)
-            criterio = abs((f_prima_evaluada*f_prima_prima_evaluada)/(f_prima_evaluada**2))
-            if criterio > 1:
-                print("El criterio de convergencia no se cumple")
-                break
-            
-        #print("La raiz aproximada es: ", x_actual)
-        instancia_respuesta.agregar_tabla()
-        res = instancia_respuesta.obtener_y_limpiar_respuesta()
-        return res
+            instancia_respuesta.agregar_titulo1("Metodo de Newton")
+            instancia_respuesta.agregar_parrafo("asdas")
+            instancia_respuesta.crear_tabla()
+            instancia_respuesta.agregar_fila(['Iteracion', 'X1', 'f(X1)', 'f\'(X1)', 'X1+1', 'Error'])
+
+
+            while True:
+                iteracion += 1
+                f_prima_evaluada = f_prima.subs(x, x_actual)
+                f_x_evaluada = f_x.subs(x, x_actual)
+                x_anterior = x_actual
+                x_actual = newton.aproximacion(f_x_evaluada, f_prima_evaluada, x_anterior)
+                error_acomulado = errores.error_aproximado_porcentual(x_anterior,x_actual)
+                
+                instancia_respuesta.agregar_fila([iteracion, x_anterior, f_x_evaluada, f_prima_evaluada, x_actual, error_acomulado])
+
+                if(error_acomulado < error_aceptado):
+                    break
+
+                #evaluar el criterio de convergencia
+                f_prima_evaluada = f_prima.subs(x, x_actual)
+                f_prima_prima_evaluada = f_prima_prima.subs(x, x_actual)
+                f_x_evaluada = f_x.subs(x, x_actual)
+                criterio = abs((f_prima_evaluada*f_prima_prima_evaluada)/(f_prima_evaluada**2))
+                if criterio > 1:
+                    print("El criterio de convergencia no se cumple")
+                    resp = instancia_respuesta.responder_error("El criterio de convergencia no se cumple")
+                    return jsonify(resp), 400
+                
+            #print("La raiz aproximada es: ", x_actual)
+            instancia_respuesta.agregar_tabla()
+            resp= instancia_respuesta.obtener_y_limpiar_respuesta()
+            return jsonify(resp), 200
+        except Exception as e:
+            resp = instancia_respuesta.responder_error("Error interno del codigo" + str(e))
+            return jsonify(resp), 500
 
 
 
--- modelos/metodos/iterativos/abiertos/secante/Secante.py
+++ modelos/metodos/iterativos/abiertos/secante/Secante.py
@@ -7,49 +7,59 @@ from .....extras.Funciones import errores, secante, respuesta_json
 class metodo_secante():
     @staticmethod
     def calcular_secante(json_data):
-        x = sp.symbols("x")
+        try:
+            x = sp.symbols("x")
+            #instanciar respuesta json
+            instancia_respuesta = respuesta_json()
 
-        #obteniendo los valores del json
-        f_x = sp.sympify(json_data["funcion"])
-        x_anterior = float(json_data["xi"])
-        x_actual = float(json_data["xu"])
-        x_calculada = 0
-
-        error_aceptado = float(json_data["tolerancia"])
-        error_acomulado = 100
-        iteracion = 0
-
-
-        #instanciar respuesta json
-        instancia_respuesta = respuesta_json()
-
-        instancia_respuesta.agregar_titulo1("Secante")
-        instancia_respuesta.agregar_parrafo("")
-        instancia_respuesta.crear_tabla()
-        instancia_respuesta.agregar_fila(['Iteracion', 'X0', 'X1', 'F(x0)', 'F(x1)', 'Xr','Error'])
-
-        #Encabezados
-       # print("{:<12s}{:<25s}{:<25s}{:<25s}{:<25s}{:<25s}{:<25s}".
-               # format("Iteracion","X0","X1","F(x0)","F(x1)","Xr","Ea%"))
-
-        while True:
-            iteracion += 1
-            f_x_evaluada_anterior = f_x.subs(x, x_anterior)
-            f_x_evaluada_actual = f_x.subs(x, x_actual)
-            x_calculada = secante.aproximacion(f_x_evaluada_anterior,f_x_evaluada_actual,x_anterior,x_actual)
-            error_acomulado = errores.error_aproximado_porcentual(x_actual,x_calculada)
-            instancia_respuesta.agregar_fila([iteracion, x_anterior, x_actual, f_x_evaluada_anterior, f_x_evaluada_actual, x_calculada, error_acomulado])
-            #print("{:<12d}{:<25s}{:<25s}{:<25s}{:<25s}{:<25s}{:<25s}".
-            #   format(iteracion,format(x_anterior),format(x_actual),format(f_x_evaluada_anterior),format(f_x_evaluada_actual),format(x_calculada),format(error_acomulado)))
-            x_anterior = x_actual
-            x_actual = x_calculada
-            if(error_acomulado < error_aceptado):
-                break
+            #Verificar la funcion obtenida
+            try:
+                #Ecuaion de la funcion
+                f_x = sp.sympify(json_data["funcion_g_x"])
+                resultado = f_x.subs(x, 2)
+                if resultado > 0:
+                    pass
+            except sp.SympifyError:
+                resp = instancia_respuesta.responder_error("Error en la funcion ingresada")
+                return jsonify(resp), 400
+            except TypeError as e:
+                resp = instancia_respuesta.responder_error("Error en la funcion ingresada")
+                return jsonify(resp), 400
+            try:
+                x_anterior = float(json_data["xi"])
+                x_actual = float(json_data["xu"])
+                error_aceptado = float(json_data["tolerancia"])
+            except ValueError as e:
+                resp = instancia_respuesta.responder_error("Error en los datos ingresados" + str(e))
+                return jsonify(resp), 400
             
-        #print("La raiz aproximada es: ", x_actual)
-        instancia_respuesta.agregar_tabla()
-        res = instancia_respuesta.obtener_y_limpiar_respuesta()
-        return res
+            x_calculada = 0
+            error_acomulado = 100
+            iteracion = 0
+
+            instancia_respuesta.agregar_titulo1("Secante")
+            instancia_respuesta.agregar_parrafo("")
+            instancia_respuesta.crear_tabla()
+            instancia_respuesta.agregar_fila(['Iteracion', 'X0', 'X1', 'F(x0)', 'F(x1)', 'Xr','Error'])
+
+            while True:
+                iteracion += 1
+                f_x_evaluada_anterior = f_x.subs(x, x_anterior)
+                f_x_evaluada_actual = f_x.subs(x, x_actual)
+                x_calculada = secante.aproximacion(f_x_evaluada_anterior,f_x_evaluada_actual,x_anterior,x_actual)
+                error_acomulado = errores.error_aproximado_porcentual(x_actual,x_calculada)
+                instancia_respuesta.agregar_fila([iteracion, x_anterior, x_actual, f_x_evaluada_anterior, f_x_evaluada_actual, x_calculada, error_acomulado])
+                x_anterior = x_actual
+                x_actual = x_calculada
+                if(error_acomulado < error_aceptado):
+                    break
+                
+            instancia_respuesta.agregar_tabla()
+            resp = instancia_respuesta.obtener_y_limpiar_respuesta()
+            return jsonify(resp), 200
+        except Exception as e:
+            resp = instancia_respuesta.responder_error("Error en el codigo:\n " + str(e) + " " + str(e.with_traceback))
+            return jsonify(resp), 500
 
 
 
