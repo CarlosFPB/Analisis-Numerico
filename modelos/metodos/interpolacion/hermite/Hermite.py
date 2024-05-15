c429c22f550825a3667d23260452c53202bfc55a
RM21041-Sion Rivas <112224852+SionRivas@users.noreply.github.com>
2024-05-15 13:44:21 -0600

    add hermite finish

--- modelos/metodos/interpolacion/hermite/Hermite.py
+++ modelos/metodos/interpolacion/hermite/Hermite.py
@@ -1,4 +1,5 @@
 import sympy as sp
+import math
 from modelos.extras.Funciones import respuesta_json
 from flask import jsonify
 
@@ -21,6 +22,20 @@ class metodo_hermite:
                 except:
                     resp = instancia_respuesta.responder_error("Error en la funcion ingresada")
                     return jsonify(resp), 400
+                try:
+                    derivada = json_data["derivada"]
+                    try:
+                        derivada = int(derivada)
+                        if derivada < 0:
+                            resp = instancia_respuesta.responder_error("la derivada debe ser un numero positivo")
+                            return jsonify(resp), 400
+                    except:
+                        resp = instancia_respuesta.responder_error("la derivada debe ser un numero entero")
+                        return jsonify(resp), 400
+
+                except:
+                    resp = instancia_respuesta.responder_error("Error en la derivada ingresada")
+                    return jsonify(resp), 400
             try:
                 matrizPuntos = json_data["matrizPuntos"]
             except:
@@ -32,12 +47,13 @@ class metodo_hermite:
             #variables
                         
             puntos_x = []
-            puntos_y = []
 
+            filas = []
 
             #validaciones
             activarerror = False
             mensajeerror = ""
+
             for i in range(len(matrizPuntos)):
                 for j in range(len(matrizPuntos[i])):
                     try:
@@ -54,121 +70,158 @@ class metodo_hermite:
                 if activarerror:
                     break
             
-            if not activarerror:
-                if len(matrizPuntos) == 0:
-                    mensajeerror = "No se ingresaron puntos"
+
+            if not tipo == 1 and not tipo == 2:
+                mensajeerror = "Error en el tipo ingresado deber se '1' o '2'"
+                activarerror = True
+            
+            if tipo == 1 and not activarerror:
+                if f_x == "":
+                    mensajeerror = "Error en la funcion ingresada"
                     activarerror = True
-                elif len(matrizPuntos) > 2:
-                    mensajeerror = "en 'Puntos' debe haber 1 lista con los puntos en X y aparte una funcion o 2 filas con los puntos en X y Y"
+                if derivada == "":
+                    mensajeerror = "Error en la derivada ingresada"
                     activarerror = True
-                elif len(matrizPuntos[0]) < 2:
-                    mensajeerror = "Debe haber al menos 2 puntos en X"
+
+                
+                if len(matrizPuntos) != 1:
+                    mensajeerror = "Al ser de tipo 1 solo se deben de enviar los puntos en X"
                     activarerror = True
-                elif tipo == 1 and len(matrizPuntos) == 2:
-                    mensajeerror = "Si se ingresa una funcion, no se deben ingresar puntos en Y"
+                elif len(set([p for p in matrizPuntos[0] if p])) != len([p for p in matrizPuntos[0] if p]):
+                    mensajeerror = "No se pueden ingresar valores repetidos en los puntos"
                     activarerror = True
-                elif tipo == 2 and len(matrizPuntos) == 1:
-                    mensajeerror = "Si es de tipo 2, se deben ingresar los puntos en X y Y"
-                    activarerror = True
-                #Caso tipo 2 con puntos en X y Y
-                elif tipo == 2 and len(matrizPuntos) == 2:
-                    #comprobar que vengan la misma cantidad de puntos en X y Y
-                    if len(matrizPuntos[0]) != len(matrizPuntos[1]):
-                        mensajeerror = "Debe haber la misma cantidad de puntos en X y Y en 'Puntos', es decir los pares ordenados"
-                        activarerror = True
-                    else:
-                        #recorrer los puntos
-                        for i in range(len(matrizPuntos[0])):
-                            #comprobar que no haya pares ordenados con valores vacios
-                            if (matrizPuntos[0][i] == "") ^ (matrizPuntos[1][i] == ""):
-                                mensajeerror = "No pueden haber pares ordenados con un valor vacio"
-                                activarerror = True
-                            #comprobar que los puntos que se guardaran no sean vacios
-                            elif not (matrizPuntos[0][i] == "" or matrizPuntos[1][i] == ""):
-                                puntos_x.append(matrizPuntos[0][i])
-                                puntos_y.append(matrizPuntos[1][i])
-                        if len(puntos_x) < 2 and not activarerror:
-                            mensajeerror = "Debe haber al menos 2 puntos en X"
-                            activarerror = True
-                #Caso tipo 1 con funcion y valores solo en X
-                elif tipo == 1 and len(matrizPuntos) == 1:
+
+                else:
                     for i in range(len(matrizPuntos[0])):
                         if not matrizPuntos[0][i] == "":
                             puntos_x.append(matrizPuntos[0][i])
-                    if len(puntos_x) < 2:
-                            mensajeerror = "Debe haber al menos 2 puntos en X"
-                            activarerror = True
+                    if len(puntos_x) == 0:
+                        mensajeerror = "No se han ingresado puntos en X"
+                        activarerror = True
                     else:
-                        puntos_y = [f_x.subs(x, i) for i in puntos_x]
-                # Verificar que no hayan valores repetidos en los puntos X
-                if len(puntos_x) != len(set(puntos_x)) and not activarerror:
-                    mensajeerror = "No pueden haber valores repetidos en los puntos en X"
-                    activarerror = True
+                        for i in puntos_x:
+                            fila = []
+                            fila.append(i)
+                            fila.append(float(f_x.subs(x, i)))
+                            funcion = f_x
+                            for j in range(derivada):
+                                funcion = sp.diff(funcion)
+                                if funcion == 0:
+                                    mensajeerror = "La derivada ingresada es mayor a la derivada de la funcion"
+                                    activarerror = True
+                                    break
+                                fila.append(float(funcion.subs(x, i)))
+                            filas.append(fila)
+                
             
+            
+            elif tipo == 2 and not activarerror:
+                if len(matrizPuntos) > 0:
+                    for i in range(len(matrizPuntos)):
+                        fila = []
+                        for j in range(len(matrizPuntos[i])):
+                            if type(matrizPuntos[i][j]) == int or type(matrizPuntos[i][j]) == float:
+                                fila.append(matrizPuntos[i][j])
+                            if not(j == len(matrizPuntos[i]) - 1):
+                                if not type(matrizPuntos[i][j]) == int or type(matrizPuntos[i][j]) == float:
+                                    break
+                        filas.append(fila)
+                        
+
 
             if activarerror:
                 resp = instancia_respuesta.responder_error(mensajeerror)
                 return jsonify(resp), 400
             else:
 
-                #En este punto inicia el metodo de lagrange
+                #En este punto inicia el metodo de hermite
                 instancia_respuesta.agregar_titulo1("Valores Iniciales")
                 if tipo == 1:
                     instancia_respuesta.agregar_clave_valor("Funcion:", f_x)
-                    instancia_respuesta.agregar_clave_valor("Puntos X:", str(puntos_x).replace("'", ""))
-                    instancia_respuesta.agregar_clave_valor("Puntos Y:", str(puntos_y).replace("'", ""))
                 else:
                     instancia_respuesta.agregar_parrafo("La funcion original se desconoce")
-                    instancia_respuesta.agregar_clave_valor("Puntos X:", str(puntos_x).replace("'", ""))
-                    instancia_respuesta.agregar_clave_valor("Puntos Y:", str(puntos_y).replace("'", ""))
-
+                instancia_respuesta.agregar_parrafo("contamos con los siguientes datos:")
+                instancia_respuesta.crear_tabla()
+                titulo = ["x", "f(x)"]
+                longitud = 0
+                for i in filas:
+                    if len(i) > longitud:
+                        longitud = len(i)
+                for i in range(2, longitud):
+                    titulo.append("D<sup>" + str(i-1) + "</sup>y")
+                instancia_respuesta.agregar_fila(titulo)
+                for i in filas:
+                    instancia_respuesta.agregar_fila(i)
+                instancia_respuesta.agregar_tabla()
                 
+
+
+
+
                 #inicio del metodo
-                     
-                def calcular_diferencia(xinicial, xfinal):
-                    if xinicial == xfinal:
-                        valor = puntos_y[xinicial]
-                        texto = "<math><mn>" + str(puntos_y[xinicial]) + "</mn></math>"
-                        return [valor, texto]
-                    else:
-                        valor = (calcular_diferencia(xinicial + 1, xfinal)[0] - calcular_diferencia(xinicial, xfinal - 1)[0]) / (
-                            puntos_x[xfinal] - puntos_x[xinicial]
-                        )
-                        texto = "<math><mfrac><mrow><mn>"+ calcular_diferencia(xinicial + 1, xfinal)[1] +"</mn><mo>-</mo><mn>"+calcular_diferencia(xinicial, xfinal - 1)[1]+"</mn></mrow><mrow><mn>"+str(puntos_x[xfinal])+"</mn><mo>-</mo><mn>"+str(puntos_x[xinicial])+"</mn></mrow></mfrac></math>"
-                        return [valor, texto]
-                    
-
-                coeficientes = [calcular_diferencia(0, i) for i in range(len(puntos_x))]
-                instancia_respuesta.agregar_titulo1("Polinomio de Interpolacion de Newton")
-
-                for i in range(len(coeficientes)):
-                    texto = "b" + str(i)+": <math>" + coeficientes[i][1] + "</math>"
-                    if i != 0:
-                        texto += "<math><mo> = </mo><mo></mo><mn>"+str(coeficientes[i][0])+"</mn></math>"
-                    instancia_respuesta.agregar_parrafo(texto)
+                instancia_respuesta.agregar_titulo1("Polinomio Interpolante de Hermite:") 
+                matris = []
+                for i in range(len(filas)):
+                    for j in range(len(filas[i]) -1):
+                        matris.append([filas[i][0], filas[i][1]])
+                for i in range(1,len(matris)):
+                    for j in range(i, len(matris)):
+                        try:
+                            valor = (matris[j][i] - matris[j-1][i]) / (matris[j][0] - matris[j-i][0])
+                            matris[j].append(valor)
+                        except:
+                            buscar_fila = [x for x in filas if x[0] == matris[j][0]]
+                            valor = buscar_fila[0][len(matris[j])] / math.factorial(i)
+                            matris[j].append(valor)
+                instancia_respuesta.agregar_parrafo("se calculan los coeficientes de la tabla de diferencias divididas")
+                instancia_respuesta.crear_tabla()
+                instancia_respuesta.agregar_fila(["x", "f(x)"])
+                for i in matris:
+                    instancia_respuesta.agregar_fila(i)
+                instancia_respuesta.agregar_tabla()
 
 
 
+
+
+                coeficientes = []
+                for i in range(len(matris)):
+                    coeficientes.append(matris[i][-1])
+                    print("b", i, " = ", matris[i][i+1])
+                print(coeficientes)
+
+                instancia_respuesta.agregar_parrafo("Se obtienen los coeficientes del polinomio interpolante de Hermite")
+                instancia_respuesta.agregar_clave_valor("Coeficientes:", str(coeficientes).replace("[", "").replace("]", ""))
                 p_x = 0
-                texto = ""
-                for i in range(len(coeficientes)):
+
+
+
+                instancia_respuesta.agregar_parrafo("Se hace la suma de los productos de los coeficientes")
+                texto = "p(x) = "
+                print("\nProducto de Newton")
+                for i in range(len(matris)):
                     producto = 1
                     for j in range(i):
-                        producto *= x - puntos_x[j]
-                    texto += str(coeficientes[i][0]) + " * [" + str(producto) + "]"
-                    if i != len(coeficientes) - 1:
+                        producto *= (x - matris[j][0])
+                    texto += str(matris[i][i+1]) + "*[" + str(producto) + "]"
+                    if i < len(matris) - 1:
                         texto += " + "
-                    p_x += coeficientes[i][0] * producto
+                    p_x += matris[i][i+1] * producto
+                instancia_respuesta.agregar_parrafo(texto)
 
-                instancia_respuesta.agregar_parrafo("se hace la suma de los productos de los coeficientes con los productos de los factores")
-                instancia_respuesta.agregar_clave_valor("p(x): ", texto)
 
+
+
+
+                p_x = sp.simplify(p_x)
                 instancia_respuesta.agregar_titulo1("Resultado")
-                instancia_respuesta.agregar_parrafo("Se simplifica la expresion obtenida:")
-                instancia_respuesta.agregar_clave_valor("p(x): ", sp.simplify(p_x))
-
+                instancia_respuesta.agregar_parrafo("El polinomio interpolante de Hermite es:")
+                instancia_respuesta.agregar_clave_valor("p(x):", p_x)
+                
                 p_x_decimal = sp.simplify(p_x.evalf())
                 instancia_respuesta.agregar_clave_valor("p(x) decimal:", str(p_x_decimal))
+
+
 
 
 
