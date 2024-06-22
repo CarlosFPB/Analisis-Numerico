d002082a6124e9398a804766b60d121f632f32c6
Fabriciot1000@github.com <MT20005@ues.edu.sv>
2024-06-22 08:34:59 -0600

    add metodos

--- modelos/unidad_cuatro/Integracion/Simpson/Simpson2/Simpson3_8.py
+++ modelos/unidad_cuatro/Integracion/Simpson/Simpson2/Simpson3_8.py
@@ -4,8 +4,7 @@ import sympy as sp
 import numpy as np
 from .....extras.Funciones import respuesta_json, verificaciones
 from .....extras.latex import conversla, conversla_html
-from .....extras.Integrales import integr_obtener, Simpson_38
-
+from .....extras.Integrales import integr_obtener, Simpson_38, verificacion_puntos_tabla
 
 class metodo_simpson:
     @staticmethod
@@ -154,3 +153,115 @@ class metodo_simpson:
         instancia_respuesta.agregar_clave_valor_segundo("Respuesta: ", I)
         resp = instancia_respuesta.obtener_y_limpiar_respuesta()
         return resp
+
+
+class metodo_simpson38_tabla():
+    @staticmethod 
+    def calcular_simpson38_tabla(matrizPuntos, tipo):
+        try:
+            instancia_respuesta = respuesta_json()
+            try:
+                #verificar si la matriz de puntos es valida
+                if not verificaciones.es_matriz(matrizPuntos):
+                    resp = instancia_respuesta.responder_error("La matriz de puntos no es valida")
+                    return jsonify(resp), 400
+            except:
+                resp = instancia_respuesta.responder_error("Error en los datos ingresados")
+                return jsonify(resp), 400
+            
+
+            #Parsear tabla y comprobar errores
+            tabla_matriz = verificacion_puntos_tabla.verificar_tabla(matrizPuntos)
+
+            if isinstance(tabla_matriz, str):
+                #Caso error
+                resp  = instancia_respuesta.responder_error(tabla_matriz)
+                return jsonify(resp), 400
+            else: 
+                puntos_x, puntos_y = tabla_matriz
+
+           
+            #Verificar si los puntos x tiene el mismo valor de h
+            incremento = float(puntos_x[1] - puntos_x[0])
+            for i in range(len(puntos_x)-1):
+                if round(puntos_x[i+1] - puntos_x[i], 10) != incremento:
+                    resp = instancia_respuesta.responder_error(f"Se encontro diferente ancho en un punto de x (el punto{puntos_x[i+1]} con el punto {puntos_x[i]})")
+                    return jsonify(resp), 400
+            #Verificar con cual tipo se tiene que resolver 
+            if tipo == "Simple":
+                resp = metodo_simpson38_tabla.simpson38_simple_tabla(puntos_x, puntos_y)
+                return resp
+            else: # Compuesto
+                resp = metodo_simpson38_tabla.simpson38_compuesto_tabla(puntos_x, puntos_y)
+                return resp
+        except Exception as e:
+            resp = instancia_respuesta.responder_error(f"Error interno en el servidor" )
+            return jsonify(resp), 500
+
+    @staticmethod
+    def simpson38_simple_tabla(puntos_x, puntos_y):
+        instancia_respuesta = respuesta_json()
+
+        if (len(puntos_x) - 1) % 3 != 0:
+            resp = instancia_respuesta.responder_error("Los datos tabulados no se ajustan con Simpson 8/3 Simple; se necesitan datos adicionales.")
+            return jsonify(resp),200
+        #Calcular trapecio simple
+        resultado_suma = 0 
+        anterior=0
+        actual=3
+        if len(puntos_x) == 4:
+            resultado = Simpson_38.simpson_simple(puntos_x[anterior], puntos_x[actual], puntos_y[anterior], puntos_y[anterior+1], puntos_y[anterior+2], puntos_y[anterior+3])
+            resultado_suma = resultado
+        else:
+            instancia_respuesta.crear_tabla()
+            instancia_respuesta.agregar_fila(["Intervalo", "Puntos x", "Puntos f(x)", "Resultado"])
+            for i in range(int(len(puntos_x)/3)):
+                resultado = Simpson_38.simpson_simple(puntos_x[anterior], puntos_x[actual], puntos_y[anterior], puntos_y[anterior+1], puntos_y[anterior+2], puntos_y[anterior+3])
+                instancia_respuesta.agregar_fila([i+1, puntos_x[anterior:actual+1], puntos_y[anterior:actual+1], resultado])
+                anterior = actual
+                actual += 3
+                resultado_suma += resultado
+            instancia_respuesta.agregar_tabla()
+        instancia_respuesta.agregar_titulo1("Metodo Simpson 3/8")
+        instancia_respuesta.agregar_clave_valor("Resultado: ", resultado_suma)
+        h = (puntos_x[1] - puntos_x[0])
+        instancia_respuesta.agregar_clave_valor("Ancho de intervalo (h): ", h)
+        resp = instancia_respuesta.obtener_y_limpiar_respuesta()
+        return jsonify(resp), 200        
+
+    @staticmethod
+    def simpson38_compuesto_tabla(puntos_x, puntos_y):
+        instancia_respuesta = respuesta_json()
+        if len(puntos_x) < 5:
+            resp = instancia_respuesta.responder_error("Se necesitan por lo menos 4 datos tabulados para resolver por Simpson 3/8 Compuesto")
+            return jsonify(resp), 400
+        
+        #Suma intermedios
+
+        #Calcular intervalos
+        n = (len(puntos_x)-1)
+        h = (puntos_x[-1] - puntos_x[0]) / n
+        suma_3i_2 = 0
+        suma_3i_1 = 0
+        suma_3i = 0
+
+        for i in range(1, n):
+            if i % 3 == 0:
+                suma_3i += puntos_y[i]  # 2*f(x_{3i})
+            elif i % 3 == 1: 
+                suma_3i_2 += puntos_y[i]  # 3*f(x_{3i-2})
+            else:
+                suma_3i_1 += puntos_y[i]  # 3*f(x_{3i-1})+ 
+        
+        resultado = Simpson_38.simpson_compuesto_tabla(puntos_y[0], suma_3i_2, suma_3i_1, suma_3i, puntos_y[-1], h)
+      
+        instancia_respuesta.agregar_titulo1("Metodo Simpson 3/8 Compuesto")
+        instancia_respuesta.agregar_clave_valor("Resultado: ", resultado)
+        instancia_respuesta.agregar_clave_valor("Suma 3i_2: ", suma_3i_2)
+        instancia_respuesta.agregar_clave_valor("Suma 3i_1: ", suma_3i_1)
+        instancia_respuesta.agregar_clave_valor("Suma 3i: ", suma_3i)
+        instancia_respuesta.agregar_clave_valor("Valor de Intervalos: ", n)
+        instancia_respuesta.agregar_clave_valor("Valor de h: ", h)
+        resp = instancia_respuesta.obtener_y_limpiar_respuesta()
+        return jsonify(resp), 200
+    
--- modelos/unidad_cuatro/Integracion/Simpson_Adaptativo/simpson_adaptativo.py
+++ modelos/unidad_cuatro/Integracion/Simpson_Adaptativo/simpson_adaptativo.py
@@ -1,28 +1,25 @@
-
 from flask import jsonify
 import sympy as sp
-import numpy as np
-from ....extras.Funciones import respuesta_json, verificaciones
+from ....extras.Funciones import respuesta_json
 from ....extras.latex import conversla, conversla_html
 from ....extras.Integrales import integr_obtener
-
 
 class simpson_adaptativo():
     @staticmethod
     def calcular_simpson_adaptativo(json_data):
-        try:
-            instancia_respuesta = respuesta_json()
+        instancia_respuesta = respuesta_json()
         
-             # Obtener integral
+        try:
+            # Obtener integral
             try:
                 f_x = conversla.latex_(json_data["latex"])
-                nivel = int(json_data["nivel"])
-            except sp.SympifyError:
+                epsilon = float(json_data["tolerancia"])
+            except (sp.SympifyError, TypeError):
                 resp = instancia_respuesta.responder_error("Error en la integral ingresada")
                 return jsonify(resp), 400
-            except TypeError as e:
+            except KeyError:
                 resp = instancia_respuesta.responder_error("Error en la integral ingresada")
-                return jsonify(resp), 400  
+                return jsonify(resp), 400
             except:
                 resp = instancia_respuesta.responder_error("No se ingreso una integral")
                 return jsonify(resp), 400
@@ -33,38 +30,53 @@ class simpson_adaptativo():
                 resp = instancia_respuesta.responder_error("La integral tiene que ser definida (contener limites)")
                 return jsonify(resp), 400
             except NameError as e:
-                resp = instancia_respuesta.responder_error("No se permiten caracteres especiales")
+                resp = instancia_respuesta.responder_error("La integral no se ingreso correctamente o se introdujo un caracter especial")
                 return jsonify(resp), 400
             except Exception as e:
                 resp = instancia_respuesta.responder_error("Error en la integral ingresada")
                 return jsonify(resp), 400
             
+            if len(variables) > 1:
+                    resp = instancia_respuesta.responder_error("La integral debe ser unidimensional")
+                    return jsonify(resp), 400
 
-            #metodo simpson adaptativo
+            instancia_respuesta.agregar_titulo1("Método Simpson Adaptativo")
+            fx1 = conversla_html.mathl_(f_x)
+            instancia_respuesta.agregar_parrafo(f"Integral {fx1}")
+            instancia_respuesta.agregar_titulo1("Datos")
 
-
-        except Exception as e:
-                resp = instancia_respuesta.responder_error(f"Error interno en el servidor" )
-                return jsonify(resp), 500
-        
-    @staticmethod
-    def recursivo_simpson(f, a, b, epsilon):
-        h = b - a
-        c = (a + b) / 2
-        s_ab = h * (f(a) + 4 * f(c) + f(b)) / 6
-        
-        # División en dos subintervalos
-        d = (a + c) / 2
-        e = (c + b) / 2
-        s_ac = (c - a) * (f(a) + 4 * f(d) + f(c)) / 6
-        s_cb = (b - c) * (f(c) + 4 * f(e) + f(b)) / 6
-        
-        if abs(s_ac + s_cb - s_ab) < 15 * epsilon:
-            print("Resultado")
-            return s_ac + s_cb + (s_ac + s_cb - s_ab) / 15
+            def f(punto):
+                return sp.N(funcion.subs(variables[0], punto))
             
-        else:
-            print("Resultado:")
-                        #Simpson izquierdo          Simpson derecho
-            return recursivo_simpson(f, a, c, epsilon/2) + recursivo_simpson(f, c, b, epsilon/2)
+            def recursivo_simpson(f, a, b, epsilon):
+                h = b - a
+                c = (a + b) / 2
+                s_ab = h * (f(a) + 4 * f(c) + f(b)) / 6
+                
+                # División en dos subintervalos
+                d = (a + c) / 2
+                e = (c + b) / 2
+                s_ac = (c - a) * (f(a) + 4 * f(d) + f(c)) / 6
+                s_cb = (b - c) * (f(c) + 4 * f(e) + f(b)) / 6
+                
+                instancia_respuesta.agregar_parrafo("Intervalo [a, b]: [{}, {}]".format(a, b))
+                instancia_respuesta.agregar_parrafo("Intervalo [a, c]: [{}, {}]".format(a, c))
+                instancia_respuesta.agregar_parrafo("Intervalo [c, b]: [{}, {}]".format(c, b))
+                
+                instancia_respuesta.agregar_parrafo("s_ab: {}".format(s_ab))
+                instancia_respuesta.agregar_parrafo("s_ac: {}".format(s_ac))
+                instancia_respuesta.agregar_parrafo("s_cb: {}".format(s_cb))
+                
+                if abs(s_ac + s_cb - s_ab) < 15 * epsilon:
+                    return s_ac + s_cb + (s_ac + s_cb - s_ab) / 15
+                else:
+                    return recursivo_simpson(f, a, c, epsilon/2) + recursivo_simpson(f, c, b, epsilon/2)
+            resultado = recursivo_simpson(f, limites[0], limites[1], epsilon)
+            
 
+            instancia_respuesta.agregar_clave_valor_segundo("Resultado", sp.N(resultado))
+            resp = instancia_respuesta.obtener_y_limpiar_respuesta()
+            return jsonify(resp), 200
+        except Exception as e:
+            resp = instancia_respuesta.responder_error("Error interno en el servidor")
+            return jsonify(resp), 500
